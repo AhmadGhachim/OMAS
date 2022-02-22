6e024f3bf5c1d4b6e94580ba374172c1b710f978
Avisheel Singh Kalsi <avi.k@usask.ca>
2022-02-21 18:48:20 -0600

    Created a Django environment under 'backend'

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""omas_backend URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/

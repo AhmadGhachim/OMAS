3a12e78bb4f8ffce5c9348dd37c03f4da6eeeb56
Posi Adeyemi <ooa642@usask.ca>
2022-02-25 14:49:49 -0600

    Displayed the heading and added functionality to choose meeting type

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [selected, setSelected] = useState("zoom")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h2>
+          OMAS
+        </h2>
+      </div>
+
+      <div className="meeting-type">
+        <form>
+          <label htmlFor="meet-type">Select Meeting Service:</label>
+          <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+            <option value="zoom">Zoom</option>
+            <option value="teams">Teams</option>
+            <option value="webex">Webex</option>
+          </select>
+        </form>
+      </div>
     </div>
   );
 }

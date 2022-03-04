74397bb31bead02ab040f30fdf29c78cd5b4aebc
AhmadGhachim <60991291+AhmadGhachim@users.noreply.github.com>
2022-03-03 20:03:20 -0600

    Added CutOff.js component and edited thr frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import MeetingType from './components/MeetingType';
 import File from './components/File';
+import CutOff from "./components/CutOff";
 
 
 function App() {
@@ -14,15 +15,44 @@ function App() {
         <h2>
           OMAS
         </h2>
+          <h3>
+              An attendance system for online meeting platforms (Zoom, Teams etc.) which uses meeting reports generated
+              by these services and automatically organises it in a database for the user.
+          </h3>
       </div>
+          <div className="steps">
+              <ol className="inside-steps" >
+                  <li>Select a meeting service that matches your platform.</li>
+                  <li>
+                      Select cutoff (Time required to be in meeting to be marked as present).
+                  </li>
+                  <li>
+                      Upload the meeting service file provided by the platform.
+                  </li>
+                  <li>
+                      Upload a file containing the student database.
+                  </li>
+                  <li>
+                      Click the process button when steps 1 to 4 are done!
+                  </li>
+              </ol>
 
-      <MeetingType />
+          </div>
+
+        <MeetingType />
+        <CutOff />
 
       <div className="meeting-documents">
         <File title="Meeting Service File" />
-        <button>Ready!</button>
         <File title="Student Database File" />
       </div>
+
+      <div className="Process_button">
+          <button>Process</button>
+      </div>
+
+
+
 
     </div>
   );

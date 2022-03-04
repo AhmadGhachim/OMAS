ee6afd2c2b89961d54fac2c152d9964fd3fbb8f3
Posi Adeyemi <ooa642@usask.ca>
2022-03-04 01:14:58 -0600

    updated the App.js and App.css to effect a change in the Process button
    made the Process button black and disabled when its state (that represents if all inputs have been satisfied) is false, and green when the state is true. Also added a state which helps in determining if the data has been processed, if true, the button then shows 'Processed', if false, it remains showing 'Process'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import MeetingType from './components/MeetingType';
 import File from './components/File';
 import CutOff from "./components/CutOff";
@@ -7,7 +7,29 @@ import CutOff from "./components/CutOff";
 
 function App() {
 
+  const [isReady, setIsReady] = useState(false); // state to use to determine if the Process button should be disabled or not.
+  const [isProcessed, setIsProcessed] = useState(false); // state to use to determine if the data has been processed and returned from the backend
 
+  const processBtnRef = useRef(null);
+
+  // placeholder function to allow the Process button to switch between disabled and non-disabled states.
+  useEffect(() => {
+    if (isReady) {
+      processBtnRef.current.className = "ready"
+      processBtnRef.current.disabled = false;
+      // processBtnRef.current.onClick = () => handleSubmit();
+    }
+    else {
+      processBtnRef.current.className = ""
+      processBtnRef.current.disabled = true;
+    }
+  }, [isReady])
+
+
+  // placeholder function to handle the onclick for the Process button
+  const handleSubmit = (e) => {
+    setIsProcessed(isProcessed => !isProcessed);
+  }
 
   return (
     <div className="App">
@@ -15,32 +37,32 @@ function App() {
         <h2>
           OMAS
         </h2>
-          <h3>
-              An attendance system for online meeting platforms (Zoom, Teams etc.) which uses meeting reports generated
-              by these services and automatically organises it in a database for the user.
-          </h3>
+        <h3>
+          An attendance system for online meeting platforms (Zoom, Teams etc.) which uses meeting reports generated
+          by these services and automatically organises it in a database for the user.
+        </h3>
       </div>
-          <div className="steps">
-              <ol className="inside-steps" >
-                  <li>Select a meeting service that matches your platform.</li>
-                  <li>
-                      Select cutoff (Time required to be in meeting to be marked as present).
-                  </li>
-                  <li>
-                      Upload the meeting service file provided by the platform.
-                  </li>
-                  <li>
-                      Upload a file containing the student database.
-                  </li>
-                  <li>
-                      Click the process button when steps 1 to 4 are done!
-                  </li>
-              </ol>
+      <div className="steps">
+        <ol className="inside-steps" >
+          <li>Select a meeting service that matches your platform.</li>
+          <li>
+            Select cutoff (Time required to be in meeting to be marked as present).
+          </li>
+          <li>
+            Upload the meeting service file provided by the platform.
+          </li>
+          <li>
+            Upload a file containing the student database.
+          </li>
+          <li>
+            Click the process button when steps 1 to 4 are done!
+          </li>
+        </ol>
 
-          </div>
+      </div>
 
-        <MeetingType />
-        <CutOff />
+      <MeetingType />
+      <CutOff />
 
       <div className="meeting-documents">
         <File title="Meeting Service File" />
@@ -48,7 +70,7 @@ function App() {
       </div>
 
       <div className="Process_button">
-          <button>Process</button>
+        <button onClick={handleSubmit} ref={processBtnRef}>{isProcessed ? "Processed" : "Process"}</button>
       </div>
 
 

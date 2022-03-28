0e44719f49686e0e9ebfc71eca03e9418fa0cca7
josh <jbr690@usask.ca>
2022-03-27 20:55:54 -0600

    Edited parse_for_zooms()

--- backend/FileParser.py
+++ backend/FileParser.py
@@ -7,8 +7,8 @@ class FileParser(object):
         self.date = None  # Meeting start date. Format -> 05/01/22
 
 
-        self.leave_time = None  # Meeting leave time. Format -> 00:34:12
-        self.leave_date = None  # Meeting leave date. Format -> 05/01/22
+       # self.leave_time = None  # Meeting leave time. Format -> 00:34:12
+       # self.leave_date = None  # Meeting leave date. Format -> 05/01/22
 
     def parse_for_teams(self):
         f = open(self.file_path, "r")
@@ -38,8 +38,8 @@ class FileParser(object):
 #####################################
     def parse_for_zooms(self):
 
-        self.leave_time = None  # Meeting start time. Format -> 00:34:12
-        self.leave_date = None  # Meeting start date. Format -> 05/01/22
+      #  self.leave_time = None  # Meeting start time. Format -> 00:34:12
+      #  self.leave_date = None  # Meeting start date. Format -> 05/01/22
         f = open(self.file_path, "r")
         try:  # copy data into python list
             for y in f:
@@ -61,6 +61,10 @@ class FileParser(object):
                  y[4] = y[4].split(", ")
                  y[4] = {'Leave Date & Time': y[4][0]}
 
+                 y[5]={'duration joined': y[5][0]}
+
+
+
                 except TypeError:
                     pass
                 except IndexError:
@@ -79,8 +83,8 @@ class FileParser(object):
         self.date = self.data[0][3]
         self.start_time = self.data[0][3]
 
-        self.leave_time = self.data[0][4] # Meeting leave time. Format -> 00:34:12
-        self.leave_date = self.data[0][4] # Meeting leave date. Format -> 05/01/22
+     #   self.leave_time = self.data[0][4] # Meeting leave time. Format -> 00:34:12
+     #   self.leave_date = self.data[0][4] # Meeting leave date. Format -> 05/01/22
 
 #####################################
     def parse_for_webex(self):
@@ -104,6 +108,6 @@ if __name__ == '__main__':
    fp0.parse_for_teams()
    fp0.__output__()
 
-   fp = FileParser("/Users/josh/PycharmProjects/bugs-for-breakfast/backend/Test files/zoom_meet_file1.csv")
+   fp = FileParser("/Users/josh/PycharmProjects/bugs-for-breakfast/backend/Test files/zoom_meet_file2.csv")
    fp.parse_for_zooms()
    fp.__output__()
